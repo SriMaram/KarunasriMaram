6765c293a4e58f7a1a084c4fd7bc2d6ef049a2c5
SriMaram <38140976+SriMaram@users.noreply.github.com>
2018-04-07 14:00:46 -0500

    Add files via upload

--- src/main/java/com/techm/ms/resource/UserResource.java
+++ src/main/java/com/techm/ms/resource/UserResource.java
@@ -1,9 +1,53 @@
 package com.techm.ms.resource;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.techm.ms.model.User;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 /**
  * This is the Interface definition for User Resource
  * 
  */
+@RequestMapping("/user")
+@Produces({ MediaType.APPLICATION_JSON })
 public interface UserResource {
-	
+
+	/**
+	 * Service definition which create the user account
+	 * 
+	 * @return Response - Returns the details of the response.
+	 */
+	@PostMapping(value = "/createUser")
+	@Produces({ MediaType.APPLICATION_JSON })
+	@Consumes({ MediaType.APPLICATION_JSON })
+	@ApiOperation(value = "Create Account Resource", notes = "Create the new user account in ResourceCollection representation format", response = Response.class)
+	@ApiResponses(value = { @ApiResponse(code = 201, message = "OK"),
+			@ApiResponse(code = 409, message = "Unable to create. A Account with name already exist") })
+	public Response createUser(@RequestBody User user);
+
+	/**
+	 * Service definition which returns the perticular user accounts
+	 * 
+	 * @return Response - Returns the details of the accounts being searched
+	 */
+	@GetMapping(value = "/getUser/{id}")
+	@Produces({ MediaType.APPLICATION_JSON })
+	@Consumes({ MediaType.APPLICATION_JSON })
+	@ApiOperation(value = "Get Account Resource", notes = "Returns all the accounts in ResourceCollection representation format", response = Response.class)
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "OK"),
+			@ApiResponse(code = 404, message = "Account with given id not found") })
+	public Response getUserAccount(@PathVariable("id") final String id);
 }
--- src/main/java/com/techm/ms/resource/UserResourceImpl.java
+++ src/main/java/com/techm/ms/resource/UserResourceImpl.java
@@ -1,8 +1,54 @@
 package com.techm.ms.resource;
 
-import org.springframework.stereotype.Controller;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
-@Controller
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.techm.ms.model.Account;
+import com.techm.ms.model.User;
+import com.techm.ms.service.AccountService;
+
+@RestController
 public class UserResourceImpl implements UserResource {
-	
+	public static final Logger logger = LoggerFactory.getLogger(UserResourceImpl.class);
+
+	@Autowired
+	AccountService accountService; 
+
+	@Override
+	public Response createUser(User user) {
+		try {
+			System.out.println(user.getId()+user.getName());
+			Account account = accountService.findByName(user.getName());
+			if (account != null) {
+				return Response.status(Status.CONFLICT).build();
+			} else {
+				accountService.saveAccount(new Account(user.getId(), user.getName()));
+				return Response.status(Status.OK).build();
+			}
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+		}
+		return null;
+	}
+
+	@Override
+	public Response getUserAccount(String id) {
+		System.out.println(id);
+		try {
+			Account account = accountService.findById(Long.valueOf(id).longValue());
+			if (account != null) {
+				return Response.status(Status.OK).build();
+			} else {
+				return Response.status(Status.NOT_FOUND).build();
+			}
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+		}
+		return null;
+	}
 }
